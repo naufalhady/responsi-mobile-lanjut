47e52981805cd4815cbdde19347ee801066252c9
Naufal Hady <m.naufalhadyaj@gmail.com>
2024-11-04 08:07:02 +0700

    Add splashscreen

---
+++ lib/splashscreen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+// import 'package:mobile_lanjut/Loginscreen.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+  super.initState();
+  Future.delayed(const Duration(seconds: 5),(){
+    Navigator.pushReplacementNamed(context, '/login');
+  });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+            "assets/images/splashscreenIcon.png",
+            width: 120.0,
+            height: 120.0,
+            fit: BoxFit.fill,
+            ),
+            const SizedBox(height: 20,),
+            const Text(
+              'Responsi',
+              style: TextStyle(
+                fontSize: 24,
+                color:  Color.fromARGB(255, 3, 28, 49),
+              ),
+            ),
+            const Text(
+              'Matakuliah Mobile Lanjut',
+              style: TextStyle(
+                fontSize: 14,
+                color: Color.fromARGB(131, 131, 131, 100),
+              ),
+            )
+          ]
+        )
+      )
+      // child: , /disini
+    );
+  }
+}
